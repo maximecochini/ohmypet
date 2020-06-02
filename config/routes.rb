49e8f01c8227c395361d1a3eb3383ba1771049f4
Jean Brisou <jean.brisou@kedgebs.com>
2020-06-02 11:53:46 +0200

    routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :pages, only [:home, :dashboard] do
-    resources :pets do
-      resources :sittings, only [:new, :create, :show, :destroy, :edit, :update]
+  resources :pages
+  resources :pets do
+    resources :sittings, only [:new, :create]
     end
-  end
+  resources :show, only [:destroy, :edit, :update]
 end
