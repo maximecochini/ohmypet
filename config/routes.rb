f9bab9e5baae8f8d114805e3d90728d2b8b47b07
Maxime Cochini <maxime.cochini@gmail.com>
2020-06-02 12:23:05 +0200

    Add : after only

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :pages
   resources :pets do
-    resources :sittings, only [:new, :create]
+    resources :sittings, only: [:new, :create]
   end
-  resources :sittings, only [:destroy, :edit, :update, :show]
+  resources :sittings, only: [:destroy, :edit, :update, :show]
 end
