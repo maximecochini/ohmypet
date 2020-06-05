ca9f518332e17db31069a9bda6f104738116a1c6
Maxime Cochini <maxime.cochini@gmail.com>
2020-06-05 14:51:31 +0200

    Bug solved

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -21,8 +21,7 @@ const initMapbox = () => {
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  markers.forEach(marker => map.setCenter([marker.lng, marker.lat]).setZoom(15));
 };
 
 export { initMapbox };
